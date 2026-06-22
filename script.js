848eca5482216b51e446a33b5852afdb1575fea6
newjuna <zxcv6309@gmail.com>
2026-06-22 20:31:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ============================================================
 // CONFIG
 // ============================================================
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbx_oiaPaaIBa7AYq8oN_G1sf0vcTrkQTY1W32U7MftAKZLsqD4wYWYe-VUzpUE0Yn1D/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwnVV8cTy8YNmBfu4DdYduyruWIA4syVgOH-G9tMZ1lpau1mAE0fnr0YYJ1HxH57_U/exec';
 
 // ============================================================
 // 평가 항목
